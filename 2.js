877c7a75d286c5fec827b84488cd2a0dfd3e3f52
Dipesh <dipeshpatro@gmail.com>
2025-05-01 20:32:11 +0530

    a

---
+++ 2.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "adadadfa@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity]);
+
+/*
+prefer not to use var
+because of issue in block issue and functional scope
+*/
